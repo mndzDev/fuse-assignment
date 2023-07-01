4e518cbd2d1d02d05b79969c56ed8c38c722d1e1
Michelle Mendez <michelle.mendez@digital-cupcake.com>
2023-06-30 21:28:18 -0400

    small fix

--- src/app/workflow/page.tsx
+++ src/app/workflow/page.tsx
@@ -6,26 +6,26 @@ import './styles.scss';
 
 export default function Workflow(){
 
-    const pages = [
-        {
-            link: '',
-            name: 'Workflow',
-            current: false
-        },
-        {
-            link: '',
-            name: 'Cart Recovery Workflow ',
-            current: true
-        },
-    ]
+    // const pages = [
+    //     {
+    //         link: '',
+    //         name: 'Workflow',
+    //         current: false
+    //     },
+    //     {
+    //         link: '',
+    //         name: 'Cart Recovery Workflow ',
+    //         current: true
+    //     },
+    // ]
 
-    const downloadButton = 
-        <ActionButton label="Download all" type="full" icon={<DownloadIcon/>  }/>
+    // const downloadButton = 
+    //     <ActionButton label="Download all" type="full" icon={<DownloadIcon/>  }/>
    
     return (
         <>
-            <Header title='Cart Recovery Workflow' breadcrumbPages={pages} button={downloadButton}/>
+            {/* <Header title='Cart Recovery Workflow' breadcrumbPages={pages} button={downloadButton}/>
             <div className="divider w-full border-b border-gray-200"></div>
-            <CartRecovery/>
+            <CartRecovery/> */}
         </>
     )   }
